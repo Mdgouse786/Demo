20303fa176ba0177eb78a525acea611042784fc5
Mdgouse786 <mdgouse4317@gmail.com>
2023-12-12 17:39:23 +0530

    third commit

---
+++ src/demo/Abc.java
@@ -0,0 +1,19 @@
+package demo;
+
+public class Abc {
+
+	public static void main(String[] args) { 
+		 String s="javadev"; 
+		 int count=0; 
+		 for(int i=0;i<s.length();i++) 
+		 { 
+		 
+		if(s.charAt(i)=='a'||s.charAt(i)=='e'||s.charAt(i)=='i'|| 
+		 s.charAt(i)=='o'||s.charAt(i)=='u') 
+		 { 
+		 System.out.println(s.charAt(i)); 
+		 count++; 
+		 }
+}
+	}
+}
